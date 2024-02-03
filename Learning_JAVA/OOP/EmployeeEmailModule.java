fc62610c48f7193b09cca1a64767dc571a4d3e6d
Udayy Singh Pawar <106630515+barrackmobamba69@users.noreply.github.com>
2024-02-03 13:49:20 +0000

    Added new classes for EmployeeEmailModule

---
+++ Learning_JAVA/OOP/EmployeeEmailModule.java
@@ -0,0 +1,49 @@
+public class EmployeeEmailModule{
+	//Nothing here for now
+}
+class Email {
+
+	private String home; //Records home email
+	private String work; //Records work email
+	
+	public Email(String home, String work) {
+		this.home = home;
+		this.work = work;
+	}
+	
+	public String getHome() { //Getters
+		return this.home;
+	}
+	
+	public String getWork() {
+		return this.work;
+	}
+	
+	public void setHome(String home) { //Setters
+		this.home = home;
+	}
+	
+	public void setWork(String work) {
+		this.work = work;
+	}
+}
+
+class Employee {
+	private String surname;
+	private String firstName;
+	private Email emailAddress;
+	
+	public Employee(String surname, String firstName, Email emailAddress) {
+		this.surname = surname;
+		this.firstName = firstName;
+	}
+	
+	public String fullName() {
+		return firstName + " " +  surname;
+	}
+	
+	public Email getEmails() {
+		return emailAddress;
+
+	}
+}
