438aea3647717376fe765a7821cfa25e144c4b38
Udayy Singh Pawar <106630515+barrackmobamba69@users.noreply.github.com>
2024-02-05 20:29:44 +0000

    Sequential program
    Calculates the frequency of even values in a large array

---
+++ Learning_JAVA/Concurrent/sss.java
@@ -0,0 +1,14 @@
+public class sss {
+	
+	public static void main(String[] args) {
+		// This code is a sequential program that calculates the frequency of even values 
+		int f[] = new int[1000000];
+		for (int j = 0; j < f.length;j++) 
+			f[j] = (int)(Math.random()*100000);
+		int freq = 0;
+		for (int j = 0; j < f.length; j++)
+			if (f[j] % 2 == 0) freq++;
+		System.out.println(freq);
+    }
+
+}
