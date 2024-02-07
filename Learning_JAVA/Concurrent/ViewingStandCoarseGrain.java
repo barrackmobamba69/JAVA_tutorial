b91367de1e6aa49a2eb263b10c3584cdb4c17fdf
Udayy Singh Pawar <106630515+barrackmobamba69@users.noreply.github.com>
2024-02-07 18:50:03 +0000

    Implemented the boiler code
    Template for coarse grain approach for Prado museum

--- Learning_JAVA/Concurrent/ViewingStandCoarseGrain.java
+++ Learning_JAVA/Concurrent/ViewingStandCoarseGrain.java
@@ -1,4 +1,18 @@
 
 public class ViewingStandCoarseGrain {
 
+	private int visitor;
+	public boolean viewing;
+	
+	public void run() {
+		
+	}
+	
+	public void findSeat() {
+		
+	}
+	
+	public void leaveSeat() {
+		
+	}
 }
