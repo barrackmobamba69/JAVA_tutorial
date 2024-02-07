e78272a6c4970b9f369e511d386a6537b1fc6a08
Udayy Singh Pawar <106630515+barrackmobamba69@users.noreply.github.com>
2024-02-07 22:11:48 +0000

    Updated locks

--- Learning_JAVA/Concurrent/ViewingStandCoarseGrain.java
+++ Learning_JAVA/Concurrent/ViewingStandCoarseGrain.java
@@ -1,18 +1,32 @@
+import java.util.concurrent.locks.*;
 
 public class ViewingStandCoarseGrain {
 
-	private int visitor;
-	public boolean viewing;
+	private int numSeats;
+	private int availableSeats;
+	public final Lock lock;
+	
+	public ViewingStandCoarseGrain(int numSeats) {
+        this.numSeats = numSeats;
+        this.availableSeats = numSeats;
+        this.lock = new ReentrantLock();
+
+	}
 	
 	public void run() {
 		
 	}
 	
 	public void findSeat() {
-		
+		numSeats++;
 	}
 	
 	public void leaveSeat() {
+		numSeats--;
+	}
+	
+	
+	public static void main (String[] args) {
 		
 	}
 }
