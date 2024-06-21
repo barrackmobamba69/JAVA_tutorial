252a8e7ebe4b2b741be55d0bf7a1cc8be56d4c76
Udayy Singh Pawar <106630515+barrackmobamba69@users.noreply.github.com>
2024-06-21 16:15:39 +0100

    Implemented Square class to print a solid square

---
+++ Learning_JAVA/CodingShapes/Square.java
@@ -0,0 +1,19 @@
+public class Square {
+
+	public static void Square (int n) {
+
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<n; j++) {
+				System.out.print("* ");
+			}
+			System.out.println("");
+
+		}
+
+	}
+	
+	public static void main(String[] args) {
+		int n = 10;
+	    Square(n);
+	}
+}
